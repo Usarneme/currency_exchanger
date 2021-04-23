43108d5526e060a29e9b1ec1e7b14b14a0d4a1c8
Usarneme <jamestcoop@gmail.com>
2021-04-23 10:42:17 -0700

    adds from and to props and methods to CurrencyExchangeer class

--- src/CurrencyExchanger.js
+++ src/CurrencyExchanger.js
@@ -1,4 +1,17 @@
 export default class CurrencyExchanger {
+  constructor(from, to) {
+    this.from = from,
+    this.to = to
+  }
+
+  changeFrom(currency) {
+    this.from = currency
+  }
+
+  changeTo(currency) {
+    this.to = currency
+  }
+
   static async getExchangeRateFor(currency) {
     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`
     const response = await fetch(url)
--- src/main.js
+++ src/main.js
@@ -3,4 +3,22 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/styles.css'
 
-console.log($)
+// handle currency from and currency to changes
+$("#currency-from").on("select change click", "option", (event) => {
+  console.log("FROM")
+  console.log(event.currentTarget.value)
+})
+
+$("#currency-to").on("select change click", "option", (event) => {
+  console.log("TO")
+  console.log(event.currentTarget.value)
+})
+// validate both from and to are selected
+// show submit button
+// handle form submission/data retrieval
+// check cache for existing exchange rate
+// if yes, use cached data to get response/rate (cache invalidation/age of results?)
+// update ui
+// if no, make api call
+// update cache to save result
+// update ui
