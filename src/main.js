c9d8ae0308f3fec8d58af1d74f6bb65c09da5b6a
Usarneme <jamestcoop@gmail.com>
2021-04-23 11:38:06 -0700

    adds an input for the original cash amount

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ const myCurrencyExchange = new CurrencyExchanger()
 
 // handle currency from and currency to changes
 $("#currency-from").on("select change click", "option", (event) => {
-  console.log("FROM", event.currentTarget.value)
   myCurrencyExchange.changeFrom(event.currentTarget.value)
+  validateReadiness()
 })
 
 $("#currency-to").on("select change click", "option", (event) => {
-  console.log("TO", event.currentTarget.value)
   myCurrencyExchange.changeTo(event.currentTarget.value)
+  validateReadiness()
 })
 
 // validate both from and to are selected
@@ -21,14 +21,19 @@ const validateReadiness = () => {
   if (myCurrencyExchange.to && myCurrencyExchange.from) showExchangeSubmitButton()
 }
 
+// show submit button
 const showExchangeSubmitButton = () => {
   $("#exchangeSubmitButton").show()
 }
-// show submit button
+
 // handle form submission/data retrieval
-// check cache for existing exchange rate
-// if yes, use cached data to get response/rate (cache invalidation/age of results?)
-// update ui
-// if no, make api call
-// update cache to save result
-// update ui
+$(".form").on("submit", event => {
+  event.preventDefault()
+  console.log("FORM SUBMIT", myCurrencyExchange)
+  // check cache for existing exchange rate
+  // if yes, use cached data to get response/rate (cache invalidation/age of results?)
+  // update ui
+  // if no, make api call
+  // update cache to save result
+  // update ui
+})
