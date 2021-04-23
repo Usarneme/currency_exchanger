1dfffe51bc35bb2943a5175ea49db810531f8d38
Usarneme <jamestcoop@gmail.com>
2021-04-23 10:48:11 -0700

    adds validation helper to ensure to and from currencies have been selected before showing the submit button

--- src/main.js
+++ src/main.js
@@ -2,18 +2,28 @@ import $ from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/styles.css'
+import CurrencyExchanger from './CurrencyExchanger'
+const myCurrencyExchange = new CurrencyExchanger()
 
 // handle currency from and currency to changes
 $("#currency-from").on("select change click", "option", (event) => {
-  console.log("FROM")
-  console.log(event.currentTarget.value)
+  console.log("FROM", event.currentTarget.value)
+  myCurrencyExchange.changeFrom(event.currentTarget.value)
 })
 
 $("#currency-to").on("select change click", "option", (event) => {
-  console.log("TO")
-  console.log(event.currentTarget.value)
+  console.log("TO", event.currentTarget.value)
+  myCurrencyExchange.changeTo(event.currentTarget.value)
 })
+
 // validate both from and to are selected
+const validateReadiness = () => {
+  if (myCurrencyExchange.to && myCurrencyExchange.from) showExchangeSubmitButton()
+}
+
+const showExchangeSubmitButton = () => {
+  $("#exchangeSubmitButton").show()
+}
 // show submit button
 // handle form submission/data retrieval
 // check cache for existing exchange rate
